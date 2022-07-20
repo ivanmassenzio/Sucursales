26b6ff8a1206488678cfaa1e503c1bab86462db6
ivan <ivanmassenzio@gmail.com>
2022-07-20 19:11:08 -0300

    se agrega OR en saludo login

--- js/login.js
+++ js/login.js
@@ -3,8 +3,8 @@ function saveData()
     let name,pswd;
     name=document.getElementById("name").value;
     pswd=document.getElementById("pswd").value;
-    console.log(name)
-    alert("Login Pass");
+     
+    alert(name || "El usuario no existe");
     localStorage.setItem('name',name);
     localStorage.setItem('pswd',pswd);
     window.location.href="/pages/home.html"
